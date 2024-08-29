ea01993c0ef42e424a176952d4852e13511e1eed
QibingJiang <qibing.jiang@ucf.edu>
2024-08-29 09:31:42 -0400

    Update scanner.py

--- scanner.py
+++ scanner.py
@@ -1,24 +1,36 @@
 # TCP Client
 import socket
 import json
+import time
 
 def start_client(host, port):
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client_socket.connect((host, port))
 
-    while(True):
-        message = input("Scan a bar code:")
-        # print(message)
-        client_socket.sendall(message.encode())
-
-    # data = client_socket.recv(1024)
-    # print(f"Received from server: {data.decode()}")
-
-    client_socket.close()
+    while True:
+        try:
+            message = input("Scan a bar code: ")
+            if message.strip() == '':  # Exit loop if input is empty
+                print("No input provided. Exiting.")
+                break
+            client_socket.sendall(message.encode())
+        except (socket.error, ConnectionResetError) as e:
+            print(f"Socket error occurred: {e}. Close TCP connection.")
+            client_socket.close()
+            break
+        except Exception as e:
+            print(f"An unexpected error occurred: {e}")
+            break
+        finally:
+            pass
 
 if __name__ == "__main__":
     with open('local_server.txt', 'r') as file_3:
         server_config = json.loads(file_3.read())
 
-    # start_server()  # Run this in one terminal
-    start_client(server_config['ip'], server_config['tcp_port'])  # Uncomment and run this in another terminal
+    while True:
+        try:
+            start_client(server_config['ip'], server_config['tcp_port'])
+        except Exception as e:
+            print(f"An unexpected error occurred: {e}. Restarting...")
+            time.sleep(1)
